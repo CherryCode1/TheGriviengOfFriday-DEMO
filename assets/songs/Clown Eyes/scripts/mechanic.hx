beeabaef56ae7e48d22af589b0d33f048dc4e4b9
AlanelCoder_we <alan.garcia.cirnes@gmail.com>
2025-07-08 01:57:52 -0600

    algunos arreglos en la mecanica del payaso aunque aun faltan cambios de balance

--- assets/songs/Clown Eyes/scripts/mechanic.hx
+++ assets/songs/Clown Eyes/scripts/mechanic.hx
@@ -1,8 +1,8 @@
 import flixel.math.FlxRect;
 
 var TIME_SPACEPRESSED:Float = 8;
-var minTime:Float = 4;
-var maxTime:Float = 8;
+var minTime:Float = 3;
+var maxTime:Float = 6;
 
 var darwingDraw = new FlxSprite();
 var barr_mechanic_ = new FlxSprite(-200, 330);
@@ -15,7 +15,7 @@ var MAX_ANXIETY:Float = 5.0;
 public var activeMechanic = true;
 
 var spaceCooldown:Float = 0;         
-var spaceCooldownTime:Float = 2.0;
+var spaceCooldownTime:Float = 3.5;
 
 function postCreate() {
     if (!FlxG.save.data.clownMechanic) return;
@@ -26,20 +26,20 @@ function postCreate() {
     barr_mechanic_.scale.set(0.5, 0.5);
     barr_mechanic_.alpha = 0;
     barr_mechanic_.clipRect = new FlxRect(0, 3, barr_mechanic_.width, barr_mechanic_.height);
-    add(barr_mechanic_);
+    insert(members.indexOf(healthBar),barr_mechanic_);
 
     barr_mechanicBG.loadGraphic(Paths.image("stages/clown/barr_clown"));
     barr_mechanicBG.camera = camHUD;
     barr_mechanicBG.scale.set(0.5, 0.5);
     barr_mechanicBG.alpha = 0;
-    add(barr_mechanicBG);
+    insert(members.indexOf(healthBar),barr_mechanicBG);
 
     darwingDraw.loadGraphic(Paths.image("stages/clown/darwingDraw"));
     darwingDraw.screenCenter();
     darwingDraw.y += 1000;
     darwingDraw.scale.set(0.5, 0.5);
     darwingDraw.camera = camHUD;
-    add(darwingDraw);
+    insert(members.indexOf(strumLines) + 10,darwingDraw);
 
     healthBarBG.x += 150;
     healthBar.x += 150;
@@ -68,7 +68,7 @@ function postUpdate(elapsed:Float) {
             TIME_SPACEPRESSED -= 0.04;
             darwingDraw.y = FlxMath.lerp(darwingDraw.y, -360, 0.1);
 
-            TOTAL_ANXIETY -= 0.05;
+            TOTAL_ANXIETY -= 0.015;
             if (TOTAL_ANXIETY < 0) TOTAL_ANXIETY = 0;
 
             if (TIME_SPACEPRESSED <= 0) {
@@ -95,17 +95,18 @@ function postUpdate(elapsed:Float) {
         }
 
         if (TOTAL_ANXIETY >= MAX_ANXIETY) {
-            health = FlxMath.lerp(health, 0, 0.005);
+            health = FlxMath.lerp(health, -0.1, 0.005);
         }
 
         var anxietyRatio:Float = FlxMath.bound(TOTAL_ANXIETY / MAX_ANXIETY, 0, 1);
         updateValueBars(anxietyRatio);
     }
+    for (item in [barr_mechanic_, barr_mechanicBG])
+        item.alpha = healthBar.alpha;
 }
 
 function updateValueBars(anxietyRatio:Float) {
-    for (item in [barr_mechanic_, barr_mechanicBG])
-        item.alpha = healthBar.alpha;
+   
 
     var barWidth = barr_mechanic_.width;
     var barHeight = barr_mechanic_.height;
